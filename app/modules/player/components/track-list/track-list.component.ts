816c11fc2648b5d7b66266bae96ea653e05b72ba
Vicky <vicky.nseng@gmail.com>
2018-08-26 22:54:28 +0200

    First view missing file

--- app/modules/player/components/track-list/track-list.component.ts
+++ app/modules/player/components/track-list/track-list.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { LogService } from '~/modules/core/services';
+import {
+  LogService,
+  AuthService,
+  DialogService
+} from '~/modules/core/services';
 import { PlayerService } from '~/modules/player/services';
 import { ITrack } from '~/modules/core/models/track.model';
 
@@ -10,13 +14,36 @@ import { ITrack } from '~/modules/core/models/track.model';
 })
 export class TracklistComponent implements OnInit {
   constructor(
+    private authService: AuthService,
+    private dialogService: DialogService,
     private logService: LogService,
     public playerService: PlayerService
   ) {}
 
   ngOnInit(): void {}
 
-  public record(track: ITrack) {
-    this.logService.inspect(track);
+  public record(track: ITrack, usernameAttempt?: string) {
+    if (AuthService.CURRENT_USER) {
+      this.dialogService
+        .confirm('Are you sure you want to re-record this track?')
+        .then(ok => {
+          if (ok) this._navToRecord(track);
+        });
+    } else {
+      this.authService
+        .promptLogin(
+          'Provide an email and password to record.',
+          usernameAttempt
+        )
+        .then(this._navToRecord.bind(this, track), usernameAttempt => {
+          // initiate sequence again
+          this.record(track, usernameAttempt);
+        });
+    }
+  }
+
+  private _navToRecord(track: ITrack) {
+    // TODO: navigate to record screen
+    this.logService.debug('yes, re-record', track);
   }
 }
